63bcc91dfc8b4ea332fe38d19f6137e233217e88
dinesh <aircrush123@gmail.com>
2023-12-17 22:06:29 +0530

    This is first commit

---
+++ src/message.js
@@ -0,0 +1,3 @@
+export const COPY_SUCCESS = "Password successfully copied to clipboard"
+export const COPY_Fail = "Copy failed: Password is not provided."
+export const COPY_FAIL_EMPTY = "Copy failed: Password is empty.";
